5ec6fe9b675548d87d13c31246598874a5b25ffe
JOMAR HONG-AN NGUYEN <jomarhan.ng@gmail.com>
2020-10-29 18:11:31 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBjtKSuidwJUu2LdtPuuFyLbwkPO50mpms",
+    apiKey: "FIREBASE_APIKEY",
     authDomain: "imessage-clone-ad53b.firebaseapp.com",
     databaseURL: "https://imessage-clone-ad53b.firebaseio.com",
     projectId: "imessage-clone-ad53b",
     storageBucket: "imessage-clone-ad53b.appspot.com",
-    messagingSenderId: "607084703154",
-    appId: "1:607084703154:web:55759ef84d676aee0108ab"
+    messagingSenderId: "FIREBASE_MSG_ID",
+    appId: "FIREBASE_APP_ID"
 })
 
 const db = firebaseApp.firestore();
