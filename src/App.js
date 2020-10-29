66454928f821612564d48660495bf1efeee09b35
Jomar HA Nguyen <jomarnguyen@Jomars-MBP.hsd1.ga.comcast.net>
2020-10-29 15:53:02 -0400

    doing be firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
+import { selectUser } from "./features/userSlice";
 import Imessage from "./Imessage/Imessage";
+import Login from "./Login/Login";
 
 function App() {
-  return (
-    <div className="app">
-      <Imessage />
-    </div>
-  );
+  const user = useSelector(selectUser);
+  return <div className="app">{user ? <Imessage /> : <Login />}</div>;
 }
 
 export default App;
