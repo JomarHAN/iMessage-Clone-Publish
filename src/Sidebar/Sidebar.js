d6a20515a88acdce324aadff7174af6cbcc7945d
Jomar HA Nguyen <jomarnguyen@Jomars-MBP.hsd1.ga.comcast.net>
2020-10-30 12:07:49 -0400

    DONE

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,4 +1,12 @@
-import { Avatar, IconButton } from "@material-ui/core";
+import {
+  Avatar,
+  Button,
+  FormControl,
+  IconButton,
+  makeStyles,
+  Modal,
+  TextField,
+} from "@material-ui/core";
 import { RateReviewOutlined, SearchOutlined } from "@material-ui/icons";
 import React, { useEffect } from "react";
 import { useState } from "react";
@@ -8,16 +16,66 @@ import db, { auth } from "../firebase";
 import SidebarChat from "../SidebarChat/SidebarChat";
 import "./Sidebar.css";
 
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 300,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 function Sidebar() {
   const user = useSelector(selectUser);
   const [chats, setChats] = useState([]);
+  const [open, setOpen] = useState(false);
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+  const [newChat, setNewChat] = useState("");
 
-  const addChats = () => {
-    const chatName = prompt("Please Enter A New Chat Room");
+  const addChats = (e) => {
+    e.preventDefault();
     db.collection("chats").add({
-      chatName: chatName,
+      chatName: newChat,
     });
+    setOpen(false);
+    setNewChat("");
   };
+
+  const handleClose = () => {
+    setOpen(false);
+    setNewChat("");
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <form>
+        <FormControl>
+          <TextField
+            value={newChat}
+            onChange={(e) => setNewChat(e.target.value)}
+            label="New Chat Name"
+          />
+          <Button onClick={addChats} className="addChatBtn">
+            Create
+          </Button>
+        </FormControl>
+      </form>
+    </div>
+  );
 
   useEffect(() => {
     db.collection("chats").onSnapshot((snapshot) =>
@@ -37,8 +95,11 @@ function Sidebar() {
           <SearchOutlined />
           <input type="text" placeholder="Search" />
         </div>
+        <Modal open={open} onClose={handleClose}>
+          {body}
+        </Modal>
         <IconButton>
-          <RateReviewOutlined onClick={addChats} />
+          <RateReviewOutlined onClick={() => setOpen(true)} />
         </IconButton>
       </div>
       <div className="sidebar__chats">
